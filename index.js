44690c4e51bc205835e46cc2bf33ced87c0b5bb1
yuzchen7 <yuzchen7@gmail.com>
2023-06-27 13:37:06 +0800

    feat : add router for insert data with post request

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const join2 = 'select umi.user_id,u.fname, u.lname, umi.image_id, umi.category_i
 app.get('/join2', async (req, res) => {
     var obj = await connections.query(join2);
     obj = obj.rows;
-    // console.log(obj);
     res.send(obj);
 });
 
@@ -37,7 +36,6 @@ const join3 = 'select umi.user_id, u.fname, u.lname, i.image_path, umi.category_
 app.get('/join3', async (req, res) => {
     var obj = await connections.query(join3);
     obj = obj.rows;
-    // console.log(obj);
     res.send(obj);
 });
 
@@ -47,4 +45,75 @@ app.get('/joinall', async (req, res) => {
     obj = obj.rows;
     // console.log(obj);
     res.send(obj);
+});
+
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
+
+app.post('/new', async (req, res) => {
+    console.log(req.body);
+
+    try {
+        await connections.query('BEGIN')
+    
+        // check if category_type is already defined
+        var res_cate = await connections.query('select * from t_Category where category_type = $1', [req.body.category_type]);
+        var category_id;
+        if (res_cate.rowCount == 0) {
+            await connections.query('insert into t_Category(category_type) values ($1)', [req.body.category_type]);
+            res_cate = await connections.query('select * from t_Category where category_type = $1', [req.body.category_type]);
+            res_cate = res_cate.rows;
+            category_id = res_cate[0].category_id;
+        } else {
+            res_cate = res_cate.rows;
+            category_id = res_cate[0].category_id;
+        }
+        console.log('category_id =', category_id);
+
+
+        // check if user is already defined
+        var res_user = await connections.query('select * from t_user where fname = $1 and lname = $2', [req.body.fname, req.body.lname]);
+        var user_id;
+        if (res_user.rowCount == 0) {
+            await connections.query('insert into t_User(fname, lname) values ($1, $2)', [req.body.fname, req.body.lname]);
+            res_user = await connections.query('select * from t_User where fname = $1 and lname = $2', [req.body.fname, req.body.lname]);
+            res_user = res_user.rows;
+            user_id = res_user[0].user_id;
+        } else {
+            res_user = res_user.rows; 
+            user_id = res_user[0].user_id;
+        }
+        console.log('user_id = ', user_id);
+
+
+        // check if image is already defined
+        var res_img = await connections.query('select * from t_image where image_path = $1', [req.body.image_path]);
+        var image_id;
+        if (res_img.rowCount == 0) {
+            await connections.query('insert into t_Image(image_path) values ($1)', [req.body.image_path]);
+            res_img = await connections.query('select * from t_Image where image_path = $1', [req.body.image_path]);
+            res_img = res_img.rows;
+            image_id = res_img[0].image_id;
+        } else {
+            res_img = res_img.rows;
+            image_id = res_img[0].image_id;
+        }
+        console.log('image_id = ', image_id);
+
+
+        // check if main table has the record
+        // if no, then insert into main table
+        var res_main =  await connections.query('select * from User_mtm_Image where user_id = $1 and image_id = $2 and category_id = $3', [user_id, image_id, category_id]);
+        if (res_main.rowCount == 0) {
+            await connections.query('insert into User_mtm_Image(user_id, image_id, category_id) values ($1, $2, $3)', [user_id, image_id, category_id]);
+        }
+
+        await connections.query('COMMIT')
+        res.status(201).send('insert new record ok');
+
+    } catch (err) {
+        await connections.query('ROLLBACK')
+        res.status(500).send('insert new record fail');
+        throw err;
+    }
 });
